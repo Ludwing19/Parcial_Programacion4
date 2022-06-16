af1b32800bcdc8b9e664602739dc40b442b53185
Kenia Márquez <98610017+KenniaMarquez@users.noreply.github.com>
2022-06-16 12:21:37 -0600

    Editado por Kenia Marquez

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,1 +1,22 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
+import { GenericView } from "../GenericView";
+import { getLanding } from "../helpers";
+import { LoginContext } from "../LoginContext";
+import { HEADERS_AUTH } from "../utils/constans";
+
+export const LandingPage = () => {
+  const [context, setContext] = useContext(LoginContext);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const history = useNavigate();
+  useEffect(() => {
+    if (context === null || user === null) {
+      history("/login");
+    }
+  }, []);
+  console.log(user);
+  return (
+    <GenericView values={getLanding()} headers={HEADERS_AUTH(user.token)} />
+  );
+};
