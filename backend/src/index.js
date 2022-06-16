4400380e22efd7f76895016dc93fe50ad937b55c
LudwingDesigner <85055386+Ludwing19@users.noreply.github.com>
2022-06-16 12:31:23 -0600

    Editado por Ludwing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,12 @@
 
+import Server from "./Server";
+import StartUp from "./Startup";
+
+const app = new StartUp(new Server());
+app
+  .Start()
+  .then(async () => {})
+  .catch((err) => {
+    console.log(err);
+    process.exit();
+  });
