316f2b8b77ac277c708303587887d7af9f3cbdd6
pipogarcia <98610219+pipogarcia@users.noreply.github.com>
2022-06-16 12:26:16 -0600

    Editado por Juan García

--- frontend/src/components/planets.jsx
+++ frontend/src/components/planets.jsx
@@ -1,1 +1,21 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { LoginContext } from "../LoginContext";
+import { GenericView } from "../GenericView";
+import { API_URL_PLANETS, HEADERS_AUTH } from "../utils/constans";
+
+export const Planets = () => {
+  const [context, setContext] = useContext(LoginContext);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const history = useNavigate();
+  useEffect(() => {
+    if (context === null || user === null) {
+      history("/login");
+    }
+  }, []);
+  return (
+    <GenericView values={API_URL_PLANETS} headers={HEADERS_AUTH(user.token)} />
+  );
+};
 
